dd159dd307d42714c274594371f149406037bb78
techcamp <ramar.art.2003gmail.com>
2026-07-15 12:20:26 +0530

    Fix ASGI for Render deployment

--- zoom_project/asgi.py
+++ zoom_project/asgi.py
@@ -1,3 +1,4 @@
+
 """
 ASGI config for zoom_project project.
 
@@ -6,7 +7,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-
+"""
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -26,3 +27,27 @@ application = ProtocolTypeRouter({
     ),
 })
 
+"""
+
+
+
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zoom_project.settings')
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+django_asgi_app = get_asgi_application()
+
+import meetings.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            meetings.routing.websocket_urlpatterns
+        )
+    ),
+})
